caf9a3c62940fcc55cbecc1732fe4e2e9faa291c
jjang051tj <jjang051tj@gmail.com>
2025-01-13 16:46:12 +0900

    init

--- src/main/java/com/jjang051/instagram/entity/Like.java
+++ src/main/java/com/jjang051/instagram/entity/Like.java
@@ -1,5 +1,10 @@
 package com.jjang051.instagram.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,4 +31,10 @@ public class Like {
   private Member member;
 
 
+
+  @CreatedDate
+  private LocalDateTime regDate;
+
+  @LastModifiedDate
+  private LocalDateTime modifyDate;
 }
