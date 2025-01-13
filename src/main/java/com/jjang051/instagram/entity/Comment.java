cc181ede10562f57bdf78d9ce614091423170cc4
jjang051tj <jjang051tj@gmail.com>
2025-01-13 10:49:00 +0900

    init

--- src/main/java/com/jjang051/instagram/entity/Comment.java
+++ src/main/java/com/jjang051/instagram/entity/Comment.java
@@ -1,15 +1,14 @@
 package com.jjang051.instagram.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.jjang051.instagram.constant.Role;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
@@ -17,52 +16,39 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class Image {
+public class Comment {
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Integer id;
 
-  private String caption;
-
-  private String imgUrl;
+  @Column(length = 300, nullable = false)
+  private String content;
 
   @ManyToOne
-  @JoinColumn(name = "userId", referencedColumnName = "userId")  //보통의 경우 많은 쪽이 주인이 된다.
+  @JoinColumn(name = "userId", referencedColumnName = "userId") 
   @JsonIgnoreProperties({"imageList"})
-  private Member member;
+  private Member member; // 한명의 멤버는 여러개의 댓글을 달 수 있다.
 
-
-  @OneToMany(mappedBy = "image")
-  @JsonIgnoreProperties({"image"})
-  @OrderBy("regDate DESC, id ASC")
-  private List<Comment> commentList;
-
+  @ManyToOne
+  @JoinColumn(name = "imageId")
+  private Image image;  // 이미지 하나에 여러개의 코켄트가 달릴 수 있다.
 
   @CreatedDate
   private LocalDateTime regDate;
 
   @LastModifiedDate
   private LocalDateTime modifyDate;
-
   
-  @Builder
-  public Image(String caption, String imgUrl, Member member) {
-    this.caption=caption;
-    this.imgUrl = imgUrl;
-    this.member = member;
-  }
+
 }
